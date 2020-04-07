e883db70ccff5f66cb4eee3f92bb5d62e156f692
Pubudu Weragoda <pubuduweragoda@protonmail.com>
2020-04-07 14:59:17 +0530

    Cleaned up some code and added some comments

--- numbergame.py
+++ numbergame.py
@@ -2,17 +2,30 @@
 
 import random
 
-print('What is your name?')
-name = str(input())
+
+#Get the user name in one line
+name = str(input('What is your name?'))
+
+
 print('Well, ' + name + ' I am thinking of a number between 1 and 100. What do you think it is?')
+
+
+# Check if the user entered a valid value
 try: guess = int(input())
 except ValueError:
     print('That is not an number. Try again')
     guess = int(input)
+
+
+# Generate random number (number that the computer is thinking
 realNum = random.randint(1,100)
 
+
+# Counts the number of attempts by the user
 i = 1
 
+
+# Check if, guessed number = computer generated number
 while (guess != realNum):
     if int(guess) == int(realNum):
         print('Good job! It only took you ' + str(i) + ' tries.')
